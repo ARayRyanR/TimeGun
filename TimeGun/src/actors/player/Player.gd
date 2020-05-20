46aa7278cf360b8b538e8998c3cf5b6f46506ebf
eduard <vercaemereduard@gmail.com>
2020-05-20 18:28:29 -0500

    player health
    + added max_health

--- TimeGun/src/actors/player/Player.gd
+++ TimeGun/src/actors/player/Player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 # @@@ PLAYER ATTRIBUTES @@@
-export var health = 100.0 # initial health
+export var max_health = 100.0 # initial health
+var current_health
 
 # @@@ MOVEMENT VARS @@@
 var velocity = Vector2.ZERO
@@ -13,6 +14,12 @@ func _input(event: InputEvent) -> void:
 		$Camera2D.zoom *= 2
 	if event.is_action_released("zoom_in"):
 		$Camera2D.zoom /= 2
+	# shooting (pressed makes it automatic)
+	if Input.is_action_pressed("shoot"):
+		shoot()
+
+func _init() -> void:
+	current_health = max_health
 
 func _ready() -> void:
 	# set this player as current player
@@ -20,7 +27,7 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	# death check
-	if health <= 0.0:
+	if current_health <= 0.0:
 		death()
 	
 	# movement
@@ -36,10 +43,6 @@ func _process(delta: float) -> void:
 	var max_angle = current_angle + angle_draft
 	if ($Body.rotation - max_angle) > 0 || (min_angle - $Body.rotation) > 0:
 		$Body.rotation = current_angle
-
-	# shooting (pressed makes it automatic)
-	if Input.is_action_pressed("shoot"):
-		shoot()
 
 func movement():
 	var direction = Vector2(
@@ -58,10 +61,10 @@ func death():
 	queue_free()
 
 func update_health_bar():
-	$HealthBar/GreenBar.scale.x = health / 100.0
+	$HealthBar/GreenBar.scale.x = current_health / max_health
 
 # triggers when something hurts the player
 func _on_HurtBox_area_entered(area: Area2D) -> void:
 	print("player damaged")
-	health -= area.get_parent()._player_damage
+	current_health -= area.get_parent()._player_damage
 	update_health_bar()
