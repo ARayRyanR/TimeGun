003fa6fd95e53bfc1c0d864cb9be28b1955dc3c7
eduard <vercaemereduard@gmail.com>
2020-05-21 17:37:26 -0500

    added arena map ruleset
    + creates an empty map with borders

--- TimeGun/src/Game.gd
+++ TimeGun/src/Game.gd
@@ -4,12 +4,12 @@ extends Layer
 var player = null
 
 func _ready() -> void:
-	create_map("regular")
+	create_map("arena")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_select"):
 		player = null
-		create_map("regular")
+		create_map("arena")
 
 # Creates a valid map
 func create_map(ruleset: String):
@@ -38,9 +38,11 @@ func _use_ruleset(ruleset: String):
 ################################################################################
 #                    MAP RULESET
 ################################################################################
+# map size
 var MAP_WIDTH  = 48
 var MAP_HEIGHT = 48
 
+# map resources
 var grass_tiles = preload("res://assets/tilesets/grass.tres")
 var wall_tiles  = preload("res://assets/tilesets/wall.tres")
 var shadow_tiles= preload("res://assets/tilesets/occlusion.tres")
@@ -48,7 +50,7 @@ var Player      = preload("res://src/actors/player/Player.tscn")
 var Swarm       = preload("res://src/actors/drones/DroneSwarm.tscn")
 
 # @@@ RULE SETS DEFINITIONS @@@
-# creates the walls, player, enemies, etc
+# the default map
 func ruleset_regular():
 	# Create map grid
 	rule_grid_size(MAP_WIDTH, MAP_HEIGHT)
@@ -93,6 +95,47 @@ func ruleset_regular():
 	var swarm = Swarm.instance()
 	swarm.global_position = pos
 	$Enemies.add_child(swarm)
+	
+	# Create wall shadows
+	var shadow_maps = rule_create_occlusion(shadow_tiles) # get grid of shadows
+	for map in shadow_maps:
+		$Floor.add_child(map)
+
+# an empty arena
+func ruleset_arena():
+	# Create map grid
+	rule_grid_size(MAP_WIDTH, MAP_HEIGHT)
+	
+	# Create floor tilemap
+	rule_ones_grid()
+	rule_set_tilesize(64, 64)
+	rule_set_variations([])
+	var floor_map = rule_build_tilemap_from_ones(grass_tiles)
+	floor_map.name = "Grass"
+	$Floor.add_child(floor_map)
+	
+	# Create walls tilemap
+	rule_zero_grid()
+	rule_border_ones()
+	rule_set_variations([[1, 50]])
+	var walls_map = rule_build_tilemap_from_ones(wall_tiles)
+	walls_map.name = "Walls"
+	$Walls.add_child(walls_map)
+	
+	# Spawn player
+	var pos = rule_get_empty_position()
+	player = Player.instance()
+	player.global_position = pos
+	# set player camera limits
+	player.get_node("Camera2D").limit_right = layer_gridx*layer_cellx
+	player.get_node("Camera2D").limit_bottom = layer_gridy*layer_celly
+	$Player.add_child(player)
+	
+	# Spawn a swarm
+	#pos = rule_get_empty_position()
+	#var swarm = Swarm.instance()
+	#swarm.global_position = pos
+	#$Enemies.add_child(swarm)
 	
 	# Create wall shadows
 	var shadow_maps = rule_create_occlusion(shadow_tiles) # get grid of shadows
