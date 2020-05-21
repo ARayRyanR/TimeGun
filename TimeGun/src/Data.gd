b98311a7529cdd5505857c06eeb26e7eb0cd43d8
eduard <vercaemereduard@gmail.com>
2020-05-21 17:03:46 -0500

    added drone values

--- TimeGun/src/Data.gd
+++ TimeGun/src/Data.gd
@@ -6,3 +6,12 @@ var player = {
 	"fire_rate":  7.0,
 	"mov_speed":  300.0
 }
+
+var drones = {
+	"move_speed": 200.0,
+	"dodge_speed": 150.0,
+	"dodge_chance": 5.0,
+	"fire_rate": 0.3,
+	"shoot_bias": 400,
+	"max_health": 100.0
+}
