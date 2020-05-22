5fd8881632a94af974a4829692ece68ab8657f81
eduard <vercaemereduard@gmail.com>
2020-05-21 22:12:05 -0500

    sounds node
    + created sounds node in game node
    + bullets create their sfx in this node

--- TimeGun/src/objects/Bullet.gd
+++ TimeGun/src/objects/Bullet.gd
@@ -2,6 +2,13 @@ extends RigidBody2D
 
 var damage
 
+func _ready() -> void:
+	# create shot soundfx
+	var sfx = AudioStreamPlayer.new()
+	sfx.stream = load("res://assets/sfx/shot.wav")
+	sfx.playing = true
+	get_tree().current_scene.get_node("Sounds").add_child(sfx)
+
 # area detects world bodies and destroys bullet
 func _on_WorldDetector_body_entered(body: Node) -> void:
 	queue_free()
--- TimeGun/src/objects/Gun.gd
+++ TimeGun/src/objects/Gun.gd
@@ -18,9 +18,6 @@ func shoot(shot_angle: float):
 	frame = 0
 	$Flash.frame = 0
 	
-	# play sfx
-	$ShotSFX.play()
-	
 	# calculate spread
 	var angle = shot_angle + spread * (randi()%200 - 100) / 100
 	
